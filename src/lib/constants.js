7da1becd93a7a28c8cf418cea00e04bde04aed8b
Allan Zhang <allanwzhang04@gmail.com>
2023-01-14 15:19:39 -0600

    roadmap page

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -12,6 +12,8 @@ export const ROUTES = {
     HOME: "/home",
     ROADMAP : "/roadmap",
     COMMUNITY : "/community",
+    UPCOMING_TASKS: "/upcoming",
+    ALL_TASKS: "/tasks",
 }
 
 export const HOME_NAV_LABELS = {
