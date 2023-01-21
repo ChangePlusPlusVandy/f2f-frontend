e06d2fbbdf2c81367c93c6d215a3ce909c33524f
Allan Zhang <allanwzhang04@gmail.com>
2023-01-21 14:33:00 -0600

    back arrow

--- src/pages/AllTasks/index.js
+++ src/pages/AllTasks/index.js
@@ -5,7 +5,7 @@ import { NavBar } from "../NavBar";
 import { ROUTES } from "../../lib/constants";
 import { useNavigate } from "react-router-dom";
 import { TaskListItem } from "../../components/TaskListItem";
-import { ReactComponent as BackArrow } from "../../svg/Vector.svg";
+import BackArrow from "../../images/Vector.png";
 
 const cx = classNames.bind(styles);
 
@@ -49,9 +49,9 @@ export const AllTasks = () => {
     <div
       style={{ overflow: "scroll", overscrollBehavior: "none", height: "92vh" }}
     >
-      {/* <div className={cx(styles.back_arrow)} onClick={() => navigate(ROUTES.ROADMAP)}>
-          <BackArrow />
-        </div> */}
+      <div className={cx(styles.back_arrow)} onClick={() => navigate(ROUTES.ROADMAP)}>
+        <img src={BackArrow} alt="" />
+      </div>
       <div style={{ textAlign: "center" }}>
         <p className={cx(styles.header)}>All Tasks</p>
         <p className={cx(styles.header, "small")}>
